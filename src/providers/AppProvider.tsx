7bcd04f820e4891aabe4c447085c6f073c65d8e6
Jayson Coppo <jayson_coppo@choicehotels.com>
2022-03-10 17:41:53 -0700

    Crafting page can transfer items from one list to the other

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -2,12 +2,18 @@ import { FC, createContext, useState, ReactNode } from "react";
 
 import User from "../models/User";
 import Item from "../models/Item";
+import CraftingProcess from "./CraftingProcessType";
 
 interface AppContextInterface {
   user: User;
   isLoggedIn: boolean;
   userItemCount: () => number;
   addToUserItems: (itemName: string) => void;
+  craftingTableItems: Item[];
+  addToCraftingTable: (itemName: string) => void;
+  removeFromcraftingTable: (itemName: string) => void;
+  craftingProcess: string;
+  updateCraftingProcess: (process: string) => void;
 }
 
 const defaultUser: User = {
@@ -20,21 +26,29 @@ const defaultState: AppContextInterface = {
   isLoggedIn: false,
   userItemCount: () => 0,
   addToUserItems: () => {},
+  craftingTableItems: [],
+  addToCraftingTable: () => {},
+  removeFromcraftingTable: () => {},
+  craftingProcess: CraftingProcess.smelt,
+  updateCraftingProcess: () => {},
 };
 
+// create context
 const AppContext = createContext<AppContextInterface>(defaultState);
 
 export type Props = {
   children?: ReactNode;
 };
 
-// create context
 const AppProvider: FC<Props> = ({ children }: Props) => {
   // the value that will be given to the context
   const [user, setUser] = useState<User>(defaultUser);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [craftingTableItems, setCraftingTableItems] = useState<Item[]>([]);
+  const [craftingProcess, setCraftingProcess] = useState<string>(CraftingProcess.smelt);
 
   const userItemCount = () => {
+    // get the sum of all Items' qty's
     if (user.items.length > 0) {
       return user.items.map((item) => item.qty).reduce((prev, next) => prev + next);
     }
@@ -42,11 +56,43 @@ const AppProvider: FC<Props> = ({ children }: Props) => {
   };
 
   const addToUserItems = (itemName: string) => {
-    const newItemList = addToItemList(itemName, user.items);
+    const newItemList = addItemToList(itemName, user.items);
     setUser({ ...user, items: newItemList });
   };
 
-  const value = { user, isLoggedIn, userItemCount, addToUserItems };
+  const addToCraftingTable = (itemName: string) => {
+    // remove from user items
+    const newUserItems = removeItemFromList(itemName, user.items);
+    setUser({ ...user, items: newUserItems });
+    // add to crafting table
+    const newItemList = addItemToList(itemName, craftingTableItems);
+    setCraftingTableItems(newItemList);
+  };
+
+  const removeFromcraftingTable = (itemName: string) => {
+    // remove from crafting table
+    const newCraftingItems = removeItemFromList(itemName, craftingTableItems);
+    setCraftingTableItems(newCraftingItems);
+    // add to user items
+    const newUserItems = addItemToList(itemName, user.items);
+    setUser({ ...user, items: newUserItems });
+  };
+
+  const updateCraftingProcess = (process: string) => {
+    setCraftingProcess(process);
+  };
+
+  const value = {
+    user,
+    isLoggedIn,
+    userItemCount,
+    addToUserItems,
+    craftingTableItems,
+    addToCraftingTable,
+    removeFromcraftingTable,
+    craftingProcess,
+    updateCraftingProcess,
+  };
 
   return (
     // the Provider gives access to the context to its children
@@ -54,12 +100,25 @@ const AppProvider: FC<Props> = ({ children }: Props) => {
   );
 };
 
-const addToItemList = (itemName: string, itemList: Item[]): Item[] => {
-  const existingItem = itemList.find((item) => item.name == itemName);
+// data logic
+
+const addItemToList = (itemName: string, itemList: Item[]): Item[] => {
+  const existingItem = itemList.slice().find((item) => item.name == itemName);
   if (existingItem) {
     existingItem.qty += 1;
   } else {
     itemList.push({ name: itemName, qty: 1 });
+  }
+  return itemList;
+};
+
+const removeItemFromList = (itemName: string, itemList: Item[]): Item[] => {
+  const existingItem = itemList.slice().find((item) => item.name == itemName);
+  if (existingItem) {
+    existingItem.qty -= 1;
+    if (existingItem.qty == 0) {
+      itemList = itemList.slice().filter((item) => item != existingItem);
+    }
   }
   return itemList;
 };
